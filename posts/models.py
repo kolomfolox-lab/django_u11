4e05404c4829e9c445c03fc40669ef20bd767ff5
GuardovKnee <a39307503@gmal.com>
2026-02-26 11:52:45 +0500

    3-normativ: Soft delete with Custom QuerySet

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class BaseQuerySet(models.QuerySet):
+    def delete(self):
+        return self.update(is_deleted=True)
+
+class DeletedManager(models.Manager):
+    def get_queryset(self):
+        return BaseQuerySet(self.model, using=self._db).filter(is_deleted=False)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = DeletedManager()
+
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        self.save()
+
+    def __str__(self):
+        return self.title
